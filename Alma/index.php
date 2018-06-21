94b144c26d300de736dedd451b15d1747731de79
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2018-06-21 14:15:55 +0300

    Add files via upload

---
+++ Alma/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="header">
+        <div class="logo_wrapper">
+            <span>
+                <img src="" alt="">
+            </span>
+        </div>
+        <div class="nav_wrapper">
+            <ul>
+                <li><a href="#">НОВОСТИ</a></li>
+                <li><a href="#">ВИДЕО</a></li>
+                <li><a href="#">СТАТЬИ/a></li>
+                <li><a href="#">ОБЗОРЫ</a></li>
+                <li><a href="#">ПРЕВЬЮ</a></li>
+            </ul>
+        </div>
+        <div class="icon_wrap">
+            <span><img src="" alt=""></span>
+            <span><img src="" alt=""></span>
+            <span><img src="" alt=""></span>
+        </div>
+    </div>
+</body>
+</html>    
